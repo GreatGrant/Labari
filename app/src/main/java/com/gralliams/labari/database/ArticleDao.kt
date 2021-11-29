f50663f3732ddc479c4fb60dd8402d36d1aeb614
Grant Williams <greatgrant116@gmail.com>
2021-11-29 13:36:32 +0100

    Created DAO class

---
+++ app/src/main/java/com/gralliams/labari/database/ArticleDao.kt
@@ -0,0 +1,19 @@
+package com.gralliams.labari.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.gralliams.labari.model.Article
+
+@Dao
+interface ArticleDao {
+
+    //Returns id of inserted article (Long)
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun upsert(article: Article): Long
+
+    @Query("SELECT * FROM articles")
+    fun getAllArticles(article: Article): LiveData<List<Article>>
+
+    @Delete
+    suspend fun delete(article: Article)
+}
