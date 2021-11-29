c9917009248e29c2038c84062d5943ebc71ce565
Grant Williams <greatgrant116@gmail.com>
2021-11-29 12:40:05 +0100

    Created NewsService.kt class

---
+++ app/src/main/java/com/gralliams/labari/api/NewsService.kt
@@ -0,0 +1,31 @@
+package com.gralliams.labari.api
+
+import com.gralliams.labari.model.NewsResponse
+import com.gralliams.labari.util.API_KEY
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+
+interface NewsService {
+    //https://newsapi.org/v2/top-headlines?country=ng&page=1&apiKey=API_KEY
+    @GET("v2/top-headlines")
+    suspend fun getBreakingNews(
+        @Query("country")
+        countryCode: String = "ng",
+        @Query("page")
+        pageNumber: Int = 1,
+        @Query("apiKey")
+        apiKey: String = API_KEY
+    ) : Response<NewsResponse>
+
+    @GET("v2/everything")
+    suspend fun searchNews(
+        @Query("q")
+        countryCode: String,
+        @Query("page")
+        pageNumber: Int = 1,
+        @Query("apiKey")
+        apiKey: String = API_KEY
+    ) : Response<NewsResponse>
+}
---
+++ app/src/main/java/com/gralliams/labari/util/constants.kt
@@ -0,0 +1,2 @@
+package com.gralliams.labari.util
+const val API_KEY = "b3babd6066a74538bca9864e5f88e6b6"
